5af4a16294ce7067a3224f3d109f94cc1e4a5788
Forrest Vaughan <forrest@v6carolina.com>
2020-10-13 22:32:48 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace ncCreate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_OpenFile = new System.Windows.Forms.Button();
+            this.Btn_NcCreate = new System.Windows.Forms.Button();
+            this.original_code = new System.Windows.Forms.RichTextBox();
+            this.converted_code = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // Btn_OpenFile
+            // 
+            this.Btn_OpenFile.Location = new System.Drawing.Point(78, 37);
+            this.Btn_OpenFile.Name = "Btn_OpenFile";
+            this.Btn_OpenFile.Size = new System.Drawing.Size(75, 36);
+            this.Btn_OpenFile.TabIndex = 0;
+            this.Btn_OpenFile.Text = "Open";
+            this.Btn_OpenFile.UseVisualStyleBackColor = true;
+            this.Btn_OpenFile.Click += new System.EventHandler(this.Btn_OpenFile_Click);
+            // 
+            // Btn_NcCreate
+            // 
+            this.Btn_NcCreate.Location = new System.Drawing.Point(199, 37);
+            this.Btn_NcCreate.Name = "Btn_NcCreate";
+            this.Btn_NcCreate.Size = new System.Drawing.Size(75, 36);
+            this.Btn_NcCreate.TabIndex = 0;
+            this.Btn_NcCreate.Text = "Create NC";
+            this.Btn_NcCreate.UseVisualStyleBackColor = true;
+            this.Btn_NcCreate.Click += new System.EventHandler(this.Btn_NcCreate_Click);
+            // 
+            // original_code
+            // 
+            this.original_code.Location = new System.Drawing.Point(12, 79);
+            this.original_code.Name = "original_code";
+            this.original_code.Size = new System.Drawing.Size(337, 359);
+            this.original_code.TabIndex = 1;
+            this.original_code.Text = "";
+            // 
+            // converted_code
+            // 
+            this.converted_code.Location = new System.Drawing.Point(451, 79);
+            this.converted_code.Name = "converted_code";
+            this.converted_code.Size = new System.Drawing.Size(337, 359);
+            this.converted_code.TabIndex = 1;
+            this.converted_code.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.converted_code);
+            this.Controls.Add(this.original_code);
+            this.Controls.Add(this.Btn_NcCreate);
+            this.Controls.Add(this.Btn_OpenFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_OpenFile;
+        private System.Windows.Forms.Button Btn_NcCreate;
+        private System.Windows.Forms.RichTextBox original_code;
+        private System.Windows.Forms.RichTextBox converted_code;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ncCreate
+{
+    public partial class Form1 : Form
+    {
+        // Declare the filename variable
+        private string ncFileName = string.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_OpenFile_Click(object sender, EventArgs e)
+        {
+            Stream myStream;
+
+            // Choose the nc file you want to edit
+            var openFileDialog1 = new OpenFileDialog();
+
+            // Filter the results so you only see .nc files and text files
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if ((myStream = openFileDialog1.OpenFile()) != null)
+                {
+                    ncFileName = openFileDialog1.FileName;
+                    original_code.Text = File.ReadAllText(ncFileName);
+                }
+            }
+        }
+
+        private void Btn_NcCreate_Click(object sender, EventArgs e)
+        {
+            // Convert the original file to a list
+            List<string> convertList = File.ReadAllLines(ncFileName).ToList();
+
+            // Get the number of lines in List
+            int convertLength = convertList.Count;
+
+            // Go through list and copy all Outside lines/arcs to outside list
+            List<string> outsideEntities = new List<string>();
+
+            for (int i=1; i< convertLength; i+=1)
+            {
+                // Get current line on list
+                string current = convertList[i];
+
+                if (current == "LINE")
+                {
+                    if (convertList[i + 4] == "INSIDE")
+                    {
+                        outsideEntities.Add("IN X" + convertList[i + 6] + " Y" + convertList[i + 8]);
+                        outsideEntities.Add("IN X" + convertList[i + 6] + " Y" + convertList[i + 8]);
+                    }
+
+                    if (convertList[i + 4] == "OUTSIDE")
+                    {
+                        outsideEntities.Add("IN X" + convertList[i + 6] + " Y" + convertList[i + 8]);
+                        outsideEntities.Add("IN X" + convertList[i + 6] + " Y" + convertList[i + 8]);
+                    }
+                }
+
+                if (current == "ARC")
+                {
+                    if (convertList[i + 4] == "INSIDE")
+                    {
+                        outsideEntities.Add("INARC X" + convertList[i + 6] + " Y" + convertList[i + 8] + " R" + convertList[i + 12]);
+                    }
+
+                    if (convertList[i + 4] == "OUTSIDE")
+                    {
+                        outsideEntities.Add("INARC X" + convertList[i + 6] + " Y" + convertList[i + 8] + " R" + convertList[i + 12]);
+                    }
+                }
+
+                if (current == "CIRCLE")
+                {
+                    if (convertList[i + 5] == "INSIDE")
+                    {
+                        outsideEntities.Add("INCIRC X" + convertList[i + 6] + " Y" + convertList[i + 8] + " R" + convertList[i + 12]);
+                    }
+
+                    if (convertList[i + 5] == "OUTSIDE")
+                    {
+                        outsideEntities.Add("INCIRC X" + convertList[i + 6] + " Y" + convertList[i + 8] + " R" + convertList[i + 12]);
+                    }
+                }
+                
+
+            }
+
+            converted_code.Lines = outsideEntities.ToArray();
+        }
+    }
+}
